ae0842a607019d721e31f3b1f662a76dbec504ce
TopCodesonly <maulikagrawal2010@gmail.com>
2023-08-13 11:32:48 +0545

    paking and unpaking

---
+++ lesson 8/3_tuples.py
@@ -0,0 +1,10 @@
+# Creating an empty tuple
+
+a=tuple()
+print(a)
+a=()
+print(a)
+a=tuple()
+
+a=tuple([1,2,3])
+print(a)
---
+++ lesson 8/paking_unpaking.py
@@ -0,0 +1,16 @@
+# tuples in python can be paked and unpaked 
+
+# paking of typle
+
+vowels = "m" "a" "u" "l" "i" "k"
+print(type(vowels))
+
+
+# unpacking
+
+a,b=1,2
+print(a) # 1
+print(b) #2
+
+a,b = 1,2,3
+print(a,b)
